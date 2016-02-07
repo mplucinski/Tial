58d807ebc4227488c64e2d47e819889e0ccc258a
Mariusz Pluciński <mplucinski@mplucinski.com>
2016-02-07 15:58:38 +0100

    Utility: make ABI module compatible with Windows

--- Tial/TialUtility/src/ABI.cpp
+++ Tial/TialUtility/src/ABI.cpp
@@ -29,11 +29,14 @@
 
 #include <boost/core/demangle.hpp>
 
-#if (BOOST_OS_UNIX || BOOST_OS_MACOS || BOOST_PLAT_MINGW)
-#include <cxxabi.h>
+#if (BOOST_OS_UNIX || BOOST_OS_MACOS)
 #include <execinfo.h>
-#else
-#error "Platform not supported"
+#elif BOOST_OS_WINDOWS
+#include <stdio.h>
+
+#define NOMINMAX
+#include <Windows.h>
+#include <Dbghelp.h>
 #endif
 
 std::string Tial::Utility::ABI::demangle(const std::string &name) {
@@ -41,7 +44,7 @@ std::string Tial::Utility::ABI::demangle(const std::string &name) {
 }
 
 void Tial::Utility::ABI::currentStackTrace(std::function<void(const std::string&)> fn, unsigned int skipLevels) {
-#if (BOOST_OS_UNIX || BOOST_OS_MACOS || BOOST_PLAT_MINGW)
+#if (BOOST_OS_UNIX || BOOST_OS_MACOS)
 	void *buffer[256];
 	int count = backtrace(buffer, sizeof(buffer));
 	std::unique_ptr<char*, std::function<void(char**)>> symbols{
@@ -85,17 +88,60 @@ void Tial::Utility::ABI::currentStackTrace(std::function<void(const std::string&
 			fn(line);
 		}
 	}
+#elif BOOST_OS_WINDOWS
+	const size_t backTraceSize = std::numeric_limits<unsigned short>::max();
+	void *backTrace[backTraceSize];
+	USHORT count = CaptureStackBackTrace(
+		skipLevels,
+		std::numeric_limits<unsigned short>::max(),
+		backTrace,
+		nullptr
+	);
+	if(count == 0) {
+		fn("<failed CaptureStackBackTrace>");
+		return;
+	}
+
+	HANDLE process = GetCurrentProcess();
+	if(!SymInitialize(process, nullptr, true)) {
+		auto error = GetLastError();
+		fn("<failed SymInitialize: "+std::to_string(error)+">");
+		return;
+	}
+
+	const size_t nameSize = 256;
+	const size_t symbolInfoSize = sizeof(SYMBOL_INFOW) + (nameSize+1)*sizeof(char);
+	std::unique_ptr<SYMBOL_INFOW, std::function<void(SYMBOL_INFOW*)>> info{
+		reinterpret_cast<SYMBOL_INFOW*>(new char[symbolInfoSize]),
+		[](SYMBOL_INFOW *info) {
+			delete[] reinterpret_cast<char*>(info);
+		}
+	};
+	memset(info.get(), 0, symbolInfoSize);
+	info->MaxNameLen = nameSize;
+	info->SizeOfStruct = sizeof(SYMBOL_INFOW);
+
+	for(unsigned int i = 0; i < count; ++i) {
+		if(!SymFromAddrW(process, reinterpret_cast<DWORD64>(backTrace[i]), 0, info.get())) {
+			auto error = GetLastError();
+			fn("<failed SymFromAddrW: "+std::to_string(error)+">");
+		} else
+			fn(Platform::Win32::wideStringUTF8Cast<char, wchar_t>(std::wstring{info->Name, info->NameLen}));
+	}
 #else
 #error "Platform not supported"
 #endif
 }
 
 std::type_index Tial::Utility::ABI::currentExceptionType() {
-#if (BOOST_OS_UNIX || BOOST_OS_MACOS || BOOST_PLAT_MINGW)
+#if (BOOST_OS_UNIX || BOOST_OS_MACOS)
 	std::type_info *info = abi::__cxa_current_exception_type();
 	if(!info)
 		return typeid(void);
 	return *info;
+#elif BOOST_OS_WINDOWS
+	fprintf(stderr, "currentExceptionType is not available on this platform\n");
+	return typeid(void);
 #else
 #error "Platform not supported"
 #endif
